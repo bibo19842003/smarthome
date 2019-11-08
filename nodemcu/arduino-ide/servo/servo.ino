76d1f472f465fb3c4fc70ae35169a661ba1f8f6e
bibo <bibo19842003@163.com>
2019-11-08 12:03:22 +0800

    Add demo: servo

---
+++ nodemcu/arduino-ide/servo/servo.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servo;
+
+void setup ()
+{
+  servo.attach(14);//PWM引脚设置，与GPIO引脚号对应.
+}
+
+void loop ()
+{
+  // To 0°
+  servo.write(0);
+  delay(1000);
+
+  // To 90°
+  servo.write(90);
+  delay(1000);
+
+  // To 180°
+  servo.write(180);
+  delay(1000);
+}
