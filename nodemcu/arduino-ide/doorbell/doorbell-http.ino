22b67cfa7b72af745564931bf9b366da41fb3254
bibo <bibo19842003@163.com>
2019-11-15 10:45:27 +0800

    Add demo: 1.http doorbell 2.BH1750

---
+++ nodemcu/arduino-ide/doorbell/doorbell-http.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+ 
+#ifndef STASSID
+#define STASSID "CMCC-jRtE"
+#define STAPSK  "gdky6zx6"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+HTTPClient http;
+String GetUrl;
+
+
+// D4(2): button 信号
+int button = 2; // 按键连接的引脚
+
+int temp = 0;
+
+
+
+void kaimen() {
+  GetUrl = "http://192.168.1.8/?ledstatus=1";
+  http.begin(GetUrl);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      //判断请求是否成功
+      if (httpCode == HTTP_CODE_OK) {
+        Serial.println("ok ok ok");
+        }
+      }
+  else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("=================================");
+  Serial.println();
+  Serial.print(F("Connecting to "));
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+  Serial.println(F("WiFi connected"));
+  
+  pinMode(button, INPUT); // 按键设置为输出
+}
+ 
+void loop() {
+
+  // button control doorbell
+  temp = digitalRead(button);
+  if (temp == LOW) {
+      kaimen();
+  }
+
+  delay(1000);//每1s调用一次 
+}
+  
