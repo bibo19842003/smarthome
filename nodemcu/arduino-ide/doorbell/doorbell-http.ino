6b580ebc75f04ba8a95d1987c80da7abfdf4945d
bibo <bibo19842003@163.com>
2019-12-05 14:28:34 +0800

    Update demo: doorbell and speaker

--- nodemcu/arduino-ide/doorbell/doorbell-http.ino
+++ nodemcu/arduino-ide/doorbell/doorbell-http.ino
@@ -1,11 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiManager.h>
- 
+#include <ESP8266mDNS.h>
+
 
 HTTPClient http;
 String GetUrl;
 
+String mdnsname = "doorbell";
+
+// n 为 mdns service host 数量
+int n;
 
 // D4(2): button 信号
 int button = D4; // 按键连接的引脚
@@ -13,21 +18,42 @@ int button = D4; // 按键连接的引脚
 int temp = 0;
 
 
+/*
+    for (int i = 0; i < n; ++i) {
+      // 打印查找到的服务具体信息
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(MDNS.hostname(i));
+      Serial.print(" (");
+      Serial.print(MDNS.IP(i));
+      Serial.print(":");
+      Serial.print(MDNS.port(i));
+      Serial.println(")");
+    }
+*/
+
 void kaimen() {
-  GetUrl = "http://192.168.1.8/?ledstatus=1";
-  http.begin(GetUrl);
-  int httpCode = http.GET();
-  if (httpCode > 0) {
+
+  for (int i = 0; i < n; ++i) {
+
+    IPAddress ip = MDNS.IP(i);
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+    GetUrl = "http://" + ipStr + "/?ledstatus=1";
+    http.begin(GetUrl);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
       Serial.printf("[HTTP] GET... code: %d\n", httpCode);
       //判断请求是否成功
       if (httpCode == HTTP_CODE_OK) {
         Serial.println("ok ok ok");
         }
       }
-  else {
+    else {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
   }
-  http.end();
+  
 }
 
 
@@ -46,11 +72,47 @@ void setup() {
   Serial.println();
   Serial.println(WiFi.localIP());
   Serial.println(F("WiFi connected"));
-  
+
   pinMode(button, INPUT); // 按键设置为输入
+  
+  if (!MDNS.begin(mdnsname)) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+  
+  //查找服务
+  // int n = MDNS.queryService("speaker", "tcp"); // Send out query for esp tcp services
+  n = MDNS.queryService("speaker", "tcp");
+  Serial.println("mDNS query done");
+  if (n == 0) {
+    Serial.println("no services found");
+  } else {
+    Serial.print(n);
+    Serial.println(" service(s) found");
+    for (int i = 0; i < n; ++i) {
+      // 打印查找到的服务具体信息
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(MDNS.hostname(i));
+      Serial.print(" (");
+      Serial.print(MDNS.IP(i));
+      Serial.print(":");
+      Serial.print(MDNS.port(i));
+      Serial.println(")");
+    }
+  }
 }
  
 void loop() {
+
+  // 添加低功耗模式，modem模式不需要设置触发类型；GPIO_PIN_INTR_LOLEVEL/GPIO_PIN_INTR_HILEVEL
+  // wifi_set_sleep_type(MODEM_SLEEP_T);
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+  gpio_pin_wakeup_enable(D4, GPIO_PIN_INTR_LOLEVEL);
+
+  MDNS.update();
+  
+  Serial.println("test test test");
 
   // button control doorbell
   temp = digitalRead(button);
@@ -60,12 +122,4 @@ void loop() {
 
   delay(1000);//每1s调用一次 
 
-  Serial.println("test test test");
-
-// 添加低功耗模式，modem模式不需要设置触发类型；GPIO_PIN_INTR_LOLEVEL/GPIO_PIN_INTR_HILEVEL
-// wifi_set_sleep_type(MODEM_SLEEP_T);
-  wifi_set_sleep_type(LIGHT_SLEEP_T);
-  gpio_pin_wakeup_enable(D4, GPIO_PIN_INTR_LOLEVEL);
-  
 }
-
--- nodemcu/arduino-ide/speaker/speaker.ino
+++ nodemcu/arduino-ide/speaker/speaker.ino
@@ -1,13 +1,15 @@
 #include <SimpleDHT.h>
 #include <Arduino.h>
-#include <U8g2lib.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <Wire.h> //IIC
 #include <math.h> 
 #include "pitches.h"
-#include <DNSServer.h>
 #include <WiFiManager.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+
+
 
 
 // pin 使用说明：
@@ -24,6 +26,21 @@
 
 // DHT11: https://blog.bbskali.cn/index.php/archives/1337/
 
+
+// 各种变量定义
+// 各种变量定义 - begin
+
+// mdns name 定义
+// String mdnsname = "dragonfly-" + String(ESP.getChipId()); //dragonfly-7914686.local
+  String mdnsname = "speaker";
+
+// http web page ,FPSTR from <WiFiManager.h>
+const char HTTP_HEADER_NEW[] PROGMEM          = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><meta http-equiv=\"refresh\" content=\"5\"><title></title>";
+const char HTTP_STYLE_NEW[] PROGMEM           = "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} </style>";
+const char HTTP_SCRIPT_NEW[] PROGMEM          = "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
+const char HTTP_HEADER_END_NEW[] PROGMEM        = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
+const char HTTP_END_NEW[] PROGMEM             = "</div></body></html>";
+
 // notes in the melody:
 int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
@@ -34,19 +51,33 @@ int wifibutton = D4;  //wifibutton
 int temp = 0;    // 用于读取按键状态的临时变量
 int speakerpin = D8;  // 用于测试
 
-
 int pinDHT11 = D6;
 SimpleDHT11 dht11(pinDHT11);
-
-
-// Create an instance of the server
-// specify the port to listen on as an argument
-ESP8266WebServer server;
-const char HTTP_HEADER_NEW[] PROGMEM          = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><meta http-equiv=\"refresh\" content=\"5\"><title></title>";
 
 // 温度 湿度
 byte temperature = 0;
 byte humidity = 0;
+
+// 各种变量定义 - end
+
+
+// ***********************************
+
+
+// 各种服务定义
+// 各种服务定义 - begin
+
+// TCP server at port 80 will respond to HTTP requests
+ESP8266WebServer server;
+
+
+
+// 各种服务定义 - end
+
+// ***********************************
+
+
+
 
 void mytone() {
     Serial.println("LED Turned ON, buzzer on");
@@ -73,9 +104,10 @@ void valsensor()
 {
   // FPSTR from <WiFiManager.h>
   String myhtml = FPSTR(HTTP_HEADER_NEW);
-  myhtml += FPSTR(HTTP_SCRIPT);
-  myhtml += FPSTR(HTTP_STYLE);
-  myhtml += FPSTR(HTTP_HEADER_END);
+  myhtml += FPSTR(HTTP_SCRIPT_NEW);
+  myhtml += FPSTR(HTTP_STYLE_NEW);
+  myhtml += FPSTR(HTTP_HEADER_END_NEW);
+
   myhtml += "Temperature: ";
   myhtml += temperature;
   myhtml += " C";
@@ -85,8 +117,7 @@ void valsensor()
   myhtml += " %";
   myhtml += "<br>";
 
-  myhtml += FPSTR(HTTP_END);
-  
+  myhtml += FPSTR(HTTP_END_NEW);  
   server.send(200,"text/html",myhtml);
 }
 
@@ -113,10 +144,6 @@ void nocss()
     digitalWrite(led,HIGH);
   }
 }
-
-
-
-
 
 
 // handleWebRequests
@@ -167,30 +194,71 @@ void clearwifiinfor() {
 }
 
 
+void mdnsresponder() {
+ 
+  IPAddress ip = WiFi.localIP();
+  String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+  String myhtml = FPSTR(HTTP_HEADER_NEW);
+  myhtml += FPSTR(HTTP_SCRIPT_NEW);
+  myhtml += FPSTR(HTTP_STYLE_NEW);
+  myhtml += FPSTR(HTTP_HEADER_END_NEW);
+  
+  myhtml += "Hello dragonfly! ";
+  myhtml += "<br>";
+  myhtml += "IP: ";
+  myhtml += ipStr;
+
+  myhtml += FPSTR(HTTP_END_NEW);
+  server.send(200,"text/html",myhtml);
+}
+
+
 void setup() {
 
   Serial.begin(115200);
   Serial.println("=================================");
-
-  // Connect to WiFi network
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("dragonfly");
-
-  // server route
-  server.on("/",nocss);
-  server.on("/sensor",valsensor);
-  server.on("/nocss",nocss);
-  server.onNotFound(handleNotFound);
-  
-  // Start the server
-  server.begin();
-  Serial.println(WiFi.localIP());
 
   // set pinmode
   pinMode(led, OUTPUT);   // LED设置为输入
   pinMode(wifibutton, INPUT); // 按键设置为输出
   pinMode(pinDHT11, OUTPUT); // 温度传感器设置为输入
   pinMode(speakerpin, OUTPUT);   // LED设置为输入
+
+  // Connect to WiFi network
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("dragonfly");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+
+  // Set up mDNS responder:
+  if (!MDNS.begin(mdnsname)) {
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  Serial.println(mdnsname);
+  //往mDNS里面注册服务
+  MDNS.addService("speaker", "tcp", 8080); 
+
+  // server route
+  server.on("/",nocss);
+  server.on("/sensor",valsensor);
+  server.on("/nocss",nocss);
+  server.on("/mdns",mdnsresponder);
+  server.onNotFound(handleNotFound);
+  
+  // Start the server
+  server.begin();
+  Serial.println(WiFi.localIP());
 
 }
 
@@ -201,8 +269,9 @@ void setup() {
 
 void loop() {
   
-  // 获取温度 湿度
+  MDNS.update();
 
+  // 获取温度 湿度
   int err = SimpleDHTErrSuccess;
   digitalWrite(pinDHT11, HIGH);
   
