f648eda786083027050238fd0c937a423ee71070
bibo <bibo19842003@163.com>
2019-12-05 10:03:44 +0800

    Update demo: mdns

--- nodemcu/arduino-ide/mdns/mdns.ino
+++ nodemcu/arduino-ide/mdns/mdns.ino
@@ -1,129 +1,205 @@
-/**
- * Demo：
- *    演示ESP8266 mDNS responder功能
- * @author 单片机菜鸟
- * @date 2019/01/28
- */
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
-#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
 #include <WiFiManager.h>
- 
-//以下三个定义为调试定义
-#define DebugBegin(baud_rate)    Serial.begin(baud_rate)
-#define DebugPrintln(message)    Serial.println(message)
-#define DebugPrint(message)    Serial.print(message)
- 
-const char* AP_SSID     = "TP-LINK_5344"; // XXXXXX -- 使用时请修改为当前你的 wifi ssid
-const char* AP_PSK = "6206908you11011010";  // XXXXXX -- 使用时请修改为当前你的 wifi 密码
-const unsigned long BAUD_RATE = 115200;// serial connection speed
- 
-//声明一下函数
-void initBasic(void);
-void initWifi(void);
-void initWebServer(void);
-void initmDNS(void);
- 
-ESP8266WebServer server(80);
- 
-/**
- * 处理根目录uri请求
- * uri:http://server_ip/
- */
-void handleRoot() {
-  DebugPrintln("handleRoot");
-  server.send(200, "text/html", "Hello From ESP8266 mDNS demo");
-}
- 
-/**
- * 处理无效uri
- * uri:http://server_ip/xxxx
- */
-void handleNotFound() {
-  DebugPrintln("handleNotFound");
-  //打印无效uri的信息 包括请求方式 请求参数
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
- 
-void setup(void) {
 
-  initBasic();
-  initWifi();
-  initWebServer();
-  initmDNS();
-}
+
+
+// 各种变量定义
+// 各种变量定义 - begin
+
+// mdns name 定义
+// String mdnsname = "dragonfly-" + String(ESP.getChipId()); //dragonfly-7914686.local
+String mdnsname = "dragonfly"; //dragonfly.local
+
+// http web page ,FPSTR from <WiFiManager.h>
+const char HTTP_HEADER_NEW[] PROGMEM          = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><meta http-equiv=\"refresh\" content=\"5\"><title></title>";
+const char HTTP_STYLE_NEW[] PROGMEM           = "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} </style>";
+const char HTTP_SCRIPT_NEW[] PROGMEM          = "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
+const char HTTP_HEADER_END_NEW[] PROGMEM        = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
+const char HTTP_END_NEW[] PROGMEM             = "</div></body></html>";
+
+
+// 各种变量定义 - end
+
+
+// ***********************************
+
+
+// 各种服务定义
+// 各种服务定义 - begin
+
+// TCP server at port 80 will respond to HTTP requests
+ESP8266WebServer server;
+
+
+
+// 各种服务定义 - end
+
+// ***********************************
+
+
+
+
+void mdnsresponder() {
  
-void loop(void) {
-  server.handleClient();
+  IPAddress ip = WiFi.localIP();
+  String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+  String myhtml = FPSTR(HTTP_HEADER_NEW);
+  myhtml += FPSTR(HTTP_SCRIPT_NEW);
+  myhtml += FPSTR(HTTP_STYLE_NEW);
+  myhtml += FPSTR(HTTP_HEADER_END_NEW);
+  
+  myhtml += "Hello dragonfly! ";
+  myhtml += "<br>";
+  myhtml += "IP: ";
+  myhtml += ipStr;
+
+  myhtml += FPSTR(HTTP_END_NEW);
+  server.send(200,"text/html",myhtml);
 }
- 
-/**
- * 初始化基础功能：波特率
- */
-void initBasic(){
-  DebugBegin(BAUD_RATE);
+
+
+
+
+void valsensor()
+{
+  String myhtml = FPSTR(HTTP_HEADER_NEW);
+  myhtml += FPSTR(HTTP_SCRIPT);
+  myhtml += FPSTR(HTTP_STYLE);
+  myhtml += FPSTR(HTTP_HEADER_END_NEW);
+  
+  myhtml += "Temperature: ";
+//  myhtml += temperature;
+  myhtml += " C";
+  myhtml += "<br>";
+  myhtml += "Humidity: ";
+//  myhtml += humidity;
+  myhtml += " %";
+  myhtml += "<br>";
+
+  myhtml += FPSTR(HTTP_END_NEW);  
+  server.send(200,"text/html",myhtml);
 }
- 
-/**
- * 初始化wifi模块：工作模式 连接网络
- */
-void initWifi(){
-//  WiFi.mode(WIFI_STA);
-//  WiFi.begin(AP_SSID, AP_PSK);
-    WiFiManager wifiManager;
+
+
+void nocss()
+{
+//  String myhtml ="<html><head><title>Smart Home</title></head><body><h1>Test Smarthome<h1><form>test led:<button type=\"submit\" name=\"testledstatus\"value=\"0\">off</button><button type=\"submit\" name=\"testledstatus\" value=\"1\">on</button></form><form>buzzer and led:<button type=\"submit\" name=\"ledstatus\" value=\"0\">off</button><button type=\"submit\" name=\"ledstatus\" value=\"1\">on</button></form><form>Wifi mode:<button type=\"submit\" name=\"wifiled\" value=\"0\">STA</button><button type=\"submit\" name=\"wifiled\" value=\"1\">STAAP</button></form></body></html>";
+  String myhtml ="<html><head><title>Smart Home</title></head><body><h1>Smart Home<h1><form>buzzer and led:<button type=\"submit\" name=\"ledstatus\" value=\"0\">off</button><button type=\"submit\" name=\"ledstatus\" value=\"1\">on</button></form></body></html>";
+
+  myhtml += "Temperature: ";
+ // myhtml += temperature;
+  myhtml += " C";
+  myhtml += "<br>";
+  myhtml += "Humidity: ";
+//  myhtml += humidity;
+  myhtml += " %";
+  myhtml += "<br>";
+  server.send(200,"text/html",myhtml);
+
+//  if (server.arg("ledstatus")=="0") {
+ //   digitalWrite(led,LOW);
+ // } else if (server.arg("ledstatus")=="1") {
+ //   mytone();
+ //   digitalWrite(led,HIGH);
+ // }
+}
+
+
+
+
+
+
+// handleWebRequests
+String getContentType(String filename){  
+  if(server.hasArg("download")) return "application/octet-stream";  
+  else if(filename.endsWith(".htm")) return "text/html";  
+  else if(filename.endsWith(".html")) return "text/html";  
+  else if(filename.endsWith(".css")) return "text/css";  
+  else if(filename.endsWith(".js")) return "application/javascript";  
+  else if(filename.endsWith(".png")) return "image/png";  
+  else if(filename.endsWith(".gif")) return "image/gif";  
+  else if(filename.endsWith(".jpg")) return "image/jpeg";  
+  else if(filename.endsWith(".ico")) return "image/x-icon";  
+  else if(filename.endsWith(".xml")) return "text/xml";  
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";  
+  else if(filename.endsWith(".zip")) return "application/x-zip";  
+  else if(filename.endsWith(".gz")) return "application/x-gzip";  
+  return "text/plain";  
+}
+
+  
+// NotFound处理 用于处理没有注册的请求地址 一般是处理一些页面请求 
+void handleNotFound() {  
+  String path = server.uri();  
+  Serial.print("load url:");  
+  Serial.println(path);  
+  String contentType = getContentType(path);  
+  String message = "File Not Found\n\n";  
+  message += "URI: ";  
+  message += server.uri();  
+  message += "\nMethod: ";  
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";  
+  message += "\nArguments: ";  
+  message += server.args();  
+  message += "\n";  
+  for ( uint8_t i = 0; i < server.args(); i++ ) {  
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";  
+  }  
+  server.send ( 404, "text/plain", message );  
+}  
+
+
+
+
+void setup(void) {
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  WiFiManager wifiManager;
   wifiManager.autoConnect("dragonfly");
-  DebugPrintln("");
- 
+  Serial.println("");
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    DebugPrint(".");
+    Serial.print(".");
   }
-  DebugPrintln("");
-  DebugPrint("Connected to ");
-  DebugPrintln(AP_SSID);
-  DebugPrint("IP address: ");
-  DebugPrintln(WiFi.localIP());
-}
- 
-/**
- * 初始化webserver
- */
-void initWebServer(){
-  //以下配置uri对应的handler
-  server.on("/", handleRoot);
-  server.on("/inline", []() {
-    DebugPrintln("handleInline");
-    server.send(200, "text/plain", "this works as well");
-  });
-  server.onNotFound(handleNotFound);
-  //启动webserver
-  server.begin();
-  DebugPrintln("HTTP server started");
-}
- 
-/**
- * 初始化mDNS
- */
-void initmDNS(){
-  if (!MDNS.begin("esp8266")) {
-    DebugPrintln("Error setting up MDNS responder!");
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+
+  // Set up mDNS responder:
+  // String mdnsname = "dragonfly-" + String(ESP.getChipId());
+  if (!MDNS.begin(mdnsname)) {
+    Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
     }
   }
-  DebugPrintln("mDNS responder started,please input http://esp8266.local/ in your browser after install Bonjour");
+  Serial.println("mDNS responder started");
+  Serial.println(mdnsname);
+
+  // server route
+  server.on("/",nocss);
+  server.on("/sensor",valsensor);
+  server.on("/nocss",nocss);
+  server.on("/mdns",mdnsresponder);
+  server.onNotFound(handleNotFound);
+
+  // Start TCP (HTTP) server
+  server.begin();
+  Serial.println("TCP server started");
+
 }
 
+void loop(void) {
 
+  MDNS.update();
+
+  // wait for http client
+  server.handleClient();
+
+
+}
